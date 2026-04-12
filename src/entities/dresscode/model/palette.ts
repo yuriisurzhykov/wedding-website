98aab8106fa5ce2bb8033c1bff9eb2a447231ec0
Yurii Surzhykov <yuriisurzhykov@gmail.com>
2026-04-12 14:43:50 -0700

    feat: update global color palette with refined hex values and improve variable naming for clarity
    Signed-off-by: Yurii Surzhykov <yuriisurzhykov@gmail.com>

--- src/entities/dresscode/model/palette.ts
+++ src/entities/dresscode/model/palette.ts
@@ -10,10 +10,10 @@ export interface PaletteColor {
  */
 export const PALETTE: PaletteColor[] = Array.from(
     new Set<PaletteColor>([
-        {key: 'dustyrose', hex: '#d29e9e', allowed: true},
-        {key: 'sage', hex: '#748469', allowed: true},
-        {key: 'champagne', hex: '#ccb195', allowed: true},
-        {key: 'umber', hex: '#74583e', allowed: true},
+        {key: 'dustyrose', hex: '#e4cdd1', allowed: true},
+        {key: 'sage', hex: '#768462', allowed: true},
+        {key: 'champagne', hex: '#dbd2c7', allowed: true},
+        {key: 'umber', hex: '#59504a', allowed: true},
         {key: 'white', hex: '#FFFFFF', allowed: false},
         {key: 'black', hex: '#000000', allowed: false},
     ])
